9050299f049b3157b4d90eb9887b351c56345492
ryo94152 <ryo94152@gmail.com>
2020-01-13 15:37:43 +0900

    devise導入
    # What
    新規登録、ログイン機能の実装
    # Why
    編集をログイン時のみにするため

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_04_070627) do
+ActiveRecord::Schema.define(version: 2020_01_12_115845) do
 
   create_table "categories", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "code"
@@ -55,6 +55,18 @@ ActiveRecord::Schema.define(version: 2020_01_04_070627) do
     t.integer "stock_month", null: false
     t.integer "stock_day", null: false
     t.index ["category_id"], name: "fk_rails_6cf2218501"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "deliveries", "customers"
